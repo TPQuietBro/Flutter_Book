a37d2d720363a4ad9d47f1b55c82896563cdb920
TPQuietBro <1186886308@qq.com>
2019-06-26 21:29:51 +0800

    update

--- lib/BookStore/BookStore.dart
+++ lib/BookStore/BookStore.dart
@@ -1,46 +1,77 @@
 import 'package:flutter/material.dart';
 
-class BookStoreHomeWidget extends StatefulWidget{
-@override
-BookStoreHomeWidgetState createState() => BookStoreHomeWidgetState();
-
-}
-class BookStoreHomeWidgetState extends State<BookStoreHomeWidget> with SingleTickerProviderStateMixin{
-TabController _tabController;
-List tabs = ['书城','书架','分类','我的'];
-
-@override
-void initState(){
-  super.initState();
-  _tabController = TabController(length: tabs.length,vsync: this);
+class BookStoreHomeWidget extends StatefulWidget {
+  @override
+  BookStoreHomeWidgetState createState() => BookStoreHomeWidgetState();
 }
 
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-  appBar: AppBar(
+class BookStoreHomeWidgetState extends State<BookStoreHomeWidget>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  List tabs = ['书城', '书架', '分类', '我的'];
+  int _index = 0;
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: tabs.length, vsync: this)
+    ..addListener((){
+      setState(() {
+        _index = _tabController.index;
+        print('index1 = $_index');
+      });
+    });
+  }
+
+  Container _getContentBar(String text) {
+    return Container(
+      alignment: Alignment.center,
+      child: Text(text,textScaleFactor: 2)
+    );
+  }
+
+  Tab _getTab(String text){
+    print('index2 = $_index');
+    return Tab(
+      child: Text(
+      text,style: TextStyle(
+        fontSize: _index == 0 ? 12 : 16
+        )
+      )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
         title: Text("书城"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.share),
-            onPressed: (){},
-          )
-        ],
         bottom: TabBar(
           controller: _tabController,
-          tabs: tabs.map((e)=> Tab(text:e)).toList(),
+          tabs: tabs.map((e) => _getTab(e)).toList(),
+          unselectedLabelColor: Colors.black,
+          // unselectedLabelStyle: TextStyle(fontSize: 12),
+          // labelStyle: TextStyle(fontSize: 14),
+          labelPadding: EdgeInsets.only(bottom: 0,top: 0),
+          indicator: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('book_images/Switch tab-line.png'),
+              alignment: Alignment.bottomCenter
+            ),
+          ),
+          indicatorSize: TabBarIndicatorSize.label,
+          // onTap: (index){
+          //   setState(() {
+          //     _index = index;
+          //   });
+          // },
         ),
       ),
       body: TabBarView(
         controller: _tabController,
-        children: tabs.map((e){
-            return Container(
-              alignment: Alignment.center,
-              child: Text(e,textScaleFactor: 2),
-            );
-          }
-        ).toList(),
+        children: tabs.map((e) {
+          return _getContentBar(e);
+        }).toList(),
       ),
-  );
-}
+    );
+  }
 }
--- lib/Shelf/BookShelf.dart
+++ lib/Shelf/BookShelf.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 
-class BookShelfWidget extends StatefulWidget{
-@override
-
-BookShelfWidgetState createState() => BookShelfWidgetState();
+class BookShelfWidget extends StatefulWidget {
+  @override
+  BookShelfWidgetState createState() => BookShelfWidgetState();
 }
-class BookShelfWidgetState extends State<BookShelfWidget>{
-@override
 
-Widget build(BuildContext context) {
-return Scaffold(
-  appBar: AppBar(
-    title: Text("书架"),
-  ),
-);
-}
+class BookShelfWidgetState extends State<BookShelfWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("书架"),
+      ),
+    );
+  }
 }
