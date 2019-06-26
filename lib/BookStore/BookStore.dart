ca1408a42e7da0ffd1d74d3960b8c40658c66c05
TPQuietBro <1186886308@qq.com>
2019-06-26 22:01:06 +0800

    update

--- lib/BookStore/BookStore.dart
+++ lib/BookStore/BookStore.dart
@@ -9,35 +9,21 @@ class BookStoreHomeWidgetState extends State<BookStoreHomeWidget>
     with SingleTickerProviderStateMixin {
   TabController _tabController;
   List tabs = ['书城', '书架', '分类', '我的'];
-  int _index = 0;
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: tabs.length, vsync: this)
-    ..addListener((){
-      setState(() {
-        _index = _tabController.index;
-        print('index1 = $_index');
-      });
-    });
+    _tabController = TabController(length: tabs.length, vsync: this);
   }
 
   Container _getContentBar(String text) {
     return Container(
       alignment: Alignment.center,
-      child: Text(text,textScaleFactor: 2)
+      child: Text(text,textScaleFactor: 2),
     );
   }
 
   Tab _getTab(String text){
-    print('index2 = $_index');
-    return Tab(
-      child: Text(
-      text,style: TextStyle(
-        fontSize: _index == 0 ? 12 : 16
-        )
-      )
-    );
+    return Tab(text: text);
   }
 
   @override
@@ -49,21 +35,15 @@ class BookStoreHomeWidgetState extends State<BookStoreHomeWidget>
           controller: _tabController,
           tabs: tabs.map((e) => _getTab(e)).toList(),
           unselectedLabelColor: Colors.black,
-          // unselectedLabelStyle: TextStyle(fontSize: 12),
-          // labelStyle: TextStyle(fontSize: 14),
-          labelPadding: EdgeInsets.only(bottom: 0,top: 0),
+          unselectedLabelStyle: TextStyle(fontSize: 12),
+          labelStyle: TextStyle(fontSize: 16),
           indicator: BoxDecoration(
             image: DecorationImage(
               image: AssetImage('book_images/Switch tab-line.png'),
               alignment: Alignment.bottomCenter
             ),
           ),
-          indicatorSize: TabBarIndicatorSize.label,
-          // onTap: (index){
-          //   setState(() {
-          //     _index = index;
-          //   });
-          // },
+          indicatorSize: TabBarIndicatorSize.label
         ),
       ),
       body: TabBarView(
